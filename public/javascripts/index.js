83af65a555464cd42be12ab857932b8dd3a538f7
Nicolae Matei <nmatei@sdl.com>
2019-03-07 20:45:55 +0200

    es6 improvements

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
-var phoneToEdit = '';
+let phoneToEdit = '';
 
-var API_URL = {
+const API_URL = {
     CREATE: 'contacts/create',
     READ: 'contacts',
     UPDATE: 'contacts/update',
@@ -12,8 +12,8 @@ if (true || location.host === "nmatei.github.io") {
     API_URL.READ = 'data/contacts.json';
 }
 
-function loadContacts() {
-    $.ajax(API_URL.READ).done(function(contacts){
+const loadContacts = () => {
+    $.ajax(API_URL.READ).done(contacts => {
         console.info('contacts loaded', contacts);
         window.globalContacts = contacts;
         displayContacts(contacts);
@@ -21,18 +21,18 @@ function loadContacts() {
 }
 
 function saveContact() {
-    var firstName = document.querySelector('input[name=firstName]').value;
-    var lastName = $('input[name=lastName]').val();
-    var phone = $('input[name=phone]').val();
+    const firstName = document.querySelector('input[name=firstName]').value;
+    const lastName = $('input[name=lastName]').val();
+    const phone = $('input[name=phone]').val();
     console.debug('saveContact...', firstName, lastName, phone);
-    
-    var actionUrl = phoneToEdit ? API_URL.UPDATE + '?id=' + phoneToEdit : API_URL.CREATE;
+
+    const actionUrl = phoneToEdit ? API_URL.UPDATE + '?id=' + phoneToEdit : API_URL.CREATE;
     
     $.post(actionUrl, {
         firstName, // shortcut from ES6 (key is the same as value variable name)
         lastName,
         phone: phone // ES5 (key = value)
-    }).done(function(response){
+    }).done(response => {
         console.warn('done create contact', response);
         phoneToEdit = '';
         if (response.success) {
@@ -42,7 +42,7 @@ function saveContact() {
 }
 
 function displayContacts(contacts) {
-    var rows = contacts.map(function(contact) {
+    var rows = contacts.map(contact => {
         //console.log('transform contact', contact);
         const phone = contact.phone;
         const info = phone.indexOf('http') === 0 ? `<a target="_blank" href="${phone}">${phone.replace('https://github.com/', '')}</a>` : phone;
@@ -65,9 +65,7 @@ function initEvents() {
     $("tbody").delegate( "a.edit", "click", function() {
         phoneToEdit = this.getAttribute('data-id');
 
-        var contact = globalContacts.find(function(contact){
-            return contact.id == phoneToEdit;
-        });
+        var contact = globalContacts.find(contact => contact.id == phoneToEdit);
         console.log('edit', phoneToEdit, contact);
         
         document.querySelector('input[name=firstName]').value = contact.firstName;
@@ -80,7 +78,7 @@ function initEvents() {
 
 function doSearch(ev) {
     // var value = document.getElementById('search').value;
-    var value = this.value.toLowerCase();
+    const value = this.value.toLowerCase();
     
     var filteredContacts = globalContacts.filter(function (contact) {
         return contact.firstName.toLowerCase().includes(value) ||
